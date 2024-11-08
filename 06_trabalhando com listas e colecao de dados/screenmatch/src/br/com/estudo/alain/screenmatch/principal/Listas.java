4c6f380891b46f78a53f2ddf3007bf031c49bf33
alainvaz <78022608+alainvaz@users.noreply.github.com>
2024-11-08 09:20:25 -0300

    Create Listas.java

---
+++ 06_trabalhando com listas e colecao de dados/screenmatch/src/br/com/estudo/alain/screenmatch/principal/Listas.java
@@ -0,0 +1,21 @@
+package br.com.estudo.alain.screenmatch.principal;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Listas {
+    public static void main(String[] args) {
+        List<String> listaPolimorfica;
+
+        listaPolimorfica = new ArrayList<>();
+        listaPolimorfica.add("Elemento 1");
+        listaPolimorfica.add("Elemento 2");
+        System.out.println("ArrayList: " + listaPolimorfica);
+
+        listaPolimorfica = new LinkedList<>();
+        listaPolimorfica.add("Elemento A");
+        listaPolimorfica.add("Elemento B");
+        System.out.println("LinkedList: " + listaPolimorfica);
+    }
+}
