3caff81aaad1719a852921bfffcd20366d6c6dfe
alainvaz <78022608+alainvaz@users.noreply.github.com>
2024-12-14 11:23:15 -0300

    Add files via upload

---
+++ 09_Trabalhando_com_Lambdas_Streams_e_Spring_Framework/screenmatch/src/main/java/br/com/alain/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.alain.screenmatch;
+
+// mvnrepository.com > buscar dependencias necessárias
+
+import br.com.alain.screenmatch.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
+	}
+
+}
