e333218dc89e0100a4cedc5292d4ecd73d2840f3
alainvaz <78022608+alainvaz@users.noreply.github.com>
2025-01-10 14:12:11 -0300

    Add files via upload

---
+++ 14_Spring Boot 3_documente-teste_e_prepare_uma_API_para_o_deploy/api/src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -0,0 +1,70 @@
+package med.voll.api.domain.consulta;
+
+// Trecho de código suprimido
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.paciente.Paciente;
+import java.time.LocalDateTime;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@EqualsAndHashCode(of = "id")
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
+
+    public Consulta(){}
+
+    public Consulta(Long id, Medico medico, Paciente paciente, LocalDateTime data){
+        this.id = id;
+        this.medico = medico;
+        this.paciente = paciente;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+}
